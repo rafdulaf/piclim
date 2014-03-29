b3b0e7a1504f915d3f2e323c97fe71e5c8b86a7d
Raphael Franchet <raphael.franchet@anyware-services.com>
2014-03-29 17:03:11 +0100

    -

--- web/login.php
+++ web/login.php
@@ -1,7 +1,8 @@
 <?php
     include '../src/authentication.php';
 
-    if (get_current_user() == null || (isset($_REQUEST['login']) && isset($_REQUEST['password']) && login($_REQUEST['login'], $_REQUEST['password']) == false))
+    $user = get_current_user();
+    if ($user == null || (isset($_REQUEST['login']) && isset($_REQUEST['password']) && login($_REQUEST['login'], $_REQUEST['password']) == false))
     {
             echo "{ authenticate: false }";
     }
