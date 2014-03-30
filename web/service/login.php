fd46431042af41f61d12cc0d2c7905fb069e5849
Raphael Franchet <raphael.franchet@anyware-services.com>
2014-03-30 22:48:42 +0200

    -

--- web/service/login.php
+++ web/service/login.php
@@ -1,6 +1,8 @@
 <?php
     include '../../src/utils.php';
 
+    header("Access-Control-Allow-Origin: *");
+
     if (getCurrentUser() == null 
         && ((!isset($_REQUEST['login']) || !isset($_REQUEST['password'])) 
             || (isset($_REQUEST['login']) && isset($_REQUEST['password']) && login($_REQUEST['login'], $_REQUEST['password']) == false)))
