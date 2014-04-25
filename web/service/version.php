2a8ae13d2eda20558120bdcf2f31b07ff85805f5
Raphael Franchet <raphael.franchet@anyware-services.com>
2014-04-25 23:11:41 +0200

    -

--- web/service/version.php
+++ web/service/version.php
@@ -1,6 +1,6 @@
 <?php
     include '../../src/utils.php';
 
-    echo "{ version: '0.1.0', initialized: " . (isUserBaseInitialized() ? "true" : "false") . " }";
+    echo "{ version: '0.1.0', initialized: " . (isUserBaseInitialized() ? "true" : "false") . " authenticated: " . (getCurrentUser() != null ? "true" : "false") . "}";
 ?>
     
