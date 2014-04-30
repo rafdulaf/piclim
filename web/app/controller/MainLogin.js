0b3ad44b494a6d5ca3b09362e4995ba6784bb61e
Raphael Franchet <raphael.franchet@anyware-services.com>
2014-04-30 13:51:51 +0200

    -

--- web/app/controller/MainLogin.js
+++ web/app/controller/MainLogin.js
@@ -248,6 +248,7 @@ Ext.define('PiClim.controller.MainLogin', {
     _userLoginSuccess: function(fullname)
     {
     	this.getUsersList().getStore().getProxy()._url = PiClim.app.url + '/' + this.getUsersList().getStore().getProxy().initialConfig.initial_url;
+    	debugger;
     	this.getUsersList().getStore().load();
     	
     	this.getMain().getTabBar().getItems().get(0).hide();
