ef809a1afdf7482baece282db003d220b2addfa5
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-04-04 23:14:57 +0200

    -

--- web/app/store/Temperatures.js
+++ web/app/store/Temperatures.js
@@ -1,16 +1,22 @@
+Ext.define('PiClim.store.TemperaturesReader', {
+   extend: "Ext.data.reader.Json",
+   
+    getData: function(data) {
+        debugger
+        return data;
+    }
+});
+
 Ext.define('PiClim.store.Temperatures', {
     extend: "Ext.data.Store",
     
-    proxy: {
-        type: "ajax",
-        url : "service/rrd.php",
-        reader: {
-            type: "json",
-            rootProperty: "data",
-            getData: function(data) {
-                debugger
-                return data;
-            }
+    config: {
+        proxy: {
+            type: "ajax",
+            url : "service/rrd.php",
+            reader: Ext.create("PiClim.store.TemperaturesReader", {
+                rootProperty: "data"
+            })
         }
     }
 });
