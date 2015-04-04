95a12ba46cc09167ffdb815b89c07644e66578a8
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-04-04 14:42:18 +0200

    temperatures as json

---
+++ src/graph/temperatures.php
@@ -0,0 +1,21 @@
+<?php
+	// Current path
+	$CURRENT_PATH = realpath(dirname(__FILE__));
+
+	// Remove existing file if necessary
+	unlink($CURRENT_PATH."/temperatures.xml");
+	
+	// Create new export
+	$cmd = "rrdtool dump ".$CURRENT_PATH."/temperatures.rrd ".$CURRENT_PATH."/temperatures.xml";
+	shell_exec($cmd);
+
+   $xslDoc = new DOMDocument();
+   $xslDoc->load($CURRENT_PATH."/xml2json.xsl");
+
+   $xmlDoc = new DOMDocument();
+   $xmlDoc->load($CURRENT_PATH."/temperatures.xml");
+
+   $proc = new XSLTProcessor();
+   $proc->importStylesheet($xslDoc);
+   echo $proc->transformToXML($xmlDoc);
+?>
