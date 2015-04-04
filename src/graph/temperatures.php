621507c3fc235939a749b0cf45cfe7b6a1996500
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-04-04 17:03:07 +0200

    -

--- src/graph/temperatures.php
+++ src/graph/temperatures.php
@@ -1,4 +1,6 @@
 <?php
+	include 'sonde.php';
+
 	function temperaturesJSON()
 	{
 		// Current path
@@ -8,18 +10,24 @@
 		unlink($CURRENT_PATH."/temperatures.xml");
 		
 		// Create new export
-		$cmd = "rrdtool dump ".$CURRENT_PATH."/temperatures.rrd ".$CURRENT_PATH."/temperatures.xml";
+		$cmd = "rrdtool xport --start -300800 --end now --json ";
+		
+		$temperatures = getTemperatures();
+		$i = 1;
+		foreach ($temperatures as $name => $temp)
+		{
+			$cmd .= "DEF:min_".$i."=temperatures.rrd:".$name.":MIN ";
+			$cmd .= "DEF:avg_".$i."=temperatures.rrd:".$name.":AVG ";
+			$cmd .= "DEF:max_".$i."=temperatures.rrd:".$name.":MAX ";
+			
+			$cmd .= "XPORT:min_".$i.":MIN_".$name." ";
+			$cmd .= "XPORT:avg_".$i.":AVG_".$name." ";
+			$cmd .= "XPORT:max_".$i.":MAX_".$name." ";
+				
+			$i++;
+		}
+		
+//		"rrdtool xport --start -300800 --end now --json DEF:x1=temperatures.rrd:temp_salon:MIN DEF:x2=temperatures.rrd:temp_salon:AVERAGE DEF:x3=temperatures.rrd:temp_salon:MAX DEF:y1=temperatures.rrd:temp_out:MIN DEF:y2=temperatures.rrd:temp_out:AVERAGE DEF:y3=temperatures.rrd:temp_out:MAX XPORT:x1:salonMin XPORT:x2:salonAvg XPORT:x3:salonMax XPORT:y1:outMin XPORT:y2:outAvg XPORT:y3:outMax > temperatures.json";
 		shell_exec($cmd);
-	
-	   $xslDoc = new DOMDocument();
-	   $xslDoc->load($CURRENT_PATH."/xml2json.xsl");
-	
-	   $xmlDoc = new DOMDocument();
-	   $xmlDoc->load($CURRENT_PATH."/temperatures.xml");
-	
-	   $proc = new XSLTProcessor();
-	   $proc->importStylesheet($xslDoc);
-	   
-	   return $proc->transformToXML($xmlDoc);
 	}
 ?>
