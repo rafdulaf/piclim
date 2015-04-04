a288b2a06f5990bf1a6a55c48b95bfccf070f907
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-04-04 17:54:35 +0200

    -

--- src/graph/temperatures.php
+++ src/graph/temperatures.php
@@ -1,31 +1,40 @@
 <?php
 	// Current path
 	$CURRENT_PATH = realpath(dirname(__FILE__));
-	include $CURRENT_PATH.'/../sonde.php';
+	include $CURRENT_PATH.'/../config.php';
 
-	function temperaturesJSON()
+	function _temperaturesJSON($i, $name)
 	{
 		// Current path
 		$CURRENT_PATH = realpath(dirname(__FILE__));
+
+		$cmd = "";
 		
-		// Remove existing file if necessaddd
+		$cmd .= "DEF:min_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":MIN ";
+		$cmd .= "DEF:avg_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":AVERAGE ";
+		$cmd .= "DEF:max_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":MAX ";
+			
+		$cmd .= "XPORT:min_".$i.":MIN_".$name." ";
+		$cmd .= "XPORT:avg_".$i.":AVG_".$name." ";
+		$cmd .= "XPORT:max_".$i.":MAX_".$name." ";
+		
+		return $cmd;
+	}
+	
+	function temperaturesJSON()
+	{
+		global $SONDES;
+		
 		// Create new export
 		$cmd = "rrdtool xport --start -300800 --end now --json ";
 		
-		$temperatures = getTemperatures();
 		$i = 1;
-		foreach ($temperatures as $name => $temp)
+        foreach ($SONDES as $Name => $Sonde)
 		{
-			$cmd .= "DEF:min_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":MIN ";
-			$cmd .= "DEF:avg_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":AVERAGE ";
-			$cmd .= "DEF:max_".$i."=".$CURRENT_PATH."/temperatures.rrd:temp_".$name.":MAX ";
-			
-			$cmd .= "XPORT:min_".$i.":MIN_".$name." ";
-			$cmd .= "XPORT:avg_".$i.":AVG_".$name." ";
-			$cmd .= "XPORT:max_".$i.":MAX_".$name." ";
-				
+			$cmd .= _temperaturesJSON($i, $name);
 			$i++;
 		}
+		$cmd .= _temperaturesJSON($i, "out");
 		
 		return shell_exec($cmd);
 	}
