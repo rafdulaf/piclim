d1c2e02e1a2666590ae157fb86ab68f736ce351f
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-04-06 20:50:12 +0200

    -

--- web/service/version.php
+++ web/service/version.php
@@ -1,6 +1,7 @@
 <?php
     include '../../src/utils.php';
 
-    echo "{ version: '0.1.0', initialized: " . (isUserBaseInitialized() ? "true" : "false") . ", authenticated: " . (getCurrentUser() != null ? "'" . getCurrentUser()["fullname"] . "'" : "false") . "}";
+    global $TEMP_COLORS;
+    echo "{ version: '0.1.0', initialized: " . (isUserBaseInitialized() ? "true" : "false") . ", authenticated: " . (getCurrentUser() != null ? "'" . getCurrentUser()["fullname"] . "'" : "false") . ", temperatures: ".json_encode($TEMP_COLORS)."}";
 ?>
     
