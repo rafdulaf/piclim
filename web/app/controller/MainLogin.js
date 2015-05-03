f1de602323a7a4476e0a61d2d760d1f24ec56598
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-05-03 15:01:11 +0200

    -

--- web/app/controller/MainLogin.js
+++ web/app/controller/MainLogin.js
@@ -517,10 +517,8 @@ Ext.define('PiClim.controller.MainLogin', {
     },
     _setTemperatures: function()
     {
-        this.getTemperaturesChartTimeAxis().setFromDate(Ext.Date.add(new Date(), Ext.Date.HOUR, this.graph.end));
-        console.info(this.getTemperaturesChartTimeAxis().getFromDate());
-        this.getTemperaturesChartTimeAxis().setToDate(Ext.Date.add(new Date(), Ext.Date.HOUR, this.graph.start)); 
+        this.getTemperaturesChartTimeAxis().setFromDate(Ext.Date.add(new Date(), Ext.Date.HOUR, this.graph.start));
+        this.getTemperaturesChartTimeAxis().setToDate(Ext.Date.add(new Date(), Ext.Date.HOUR, this.graph.end)); 
         this.getTemperaturesChart().getStore().load({ params: { startDate: this.graph.start, endDate: this.graph.end } });
-        //this.getTemperaturesChart().redraw();
     }
 });
